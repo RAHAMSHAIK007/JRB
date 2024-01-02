df47e6aead2a75b980bfbe01ed15b7ead9438ee7
summu97 <135579543+summu97@users.noreply.github.com>
2024-01-02 15:08:11 +0530

    Update metric-server.sh

--- metric-server.sh
+++ metric-server.sh
@@ -1,10 +1,9 @@
-kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
-
-
 FOR MINIKUBE:
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 minikube addons enable metrics-server #(only for minikube)
 
 FOR KOPS:
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/high-availability-1.21+.yaml
 
 kubectl top nodes
